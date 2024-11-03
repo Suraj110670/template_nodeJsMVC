5037cf4417a96a33ebb6fc8e2a802765ce766cc6
Suraj Jugraj <suraj.jugraj@gmail.com>
2024-11-03 13:31:56 +0200

    update db.js-2

--- db.js
+++ db.js
@@ -9,10 +9,4 @@ const pool = new Pool({
   port: 5432,
 });
 
-// Confirm connection to database
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res);
-    pool.end();
-  });
-  
 module.exports = pool;
